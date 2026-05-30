c55120f50aa4c0bd6acb1c56e47cb121f824cc60
janhavvv <janhaviravindramore@gmail.com>
2026-05-30 23:54:34 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -108,7 +108,7 @@ document.addEventListener("DOMContentLoaded", function () {
         window.open("index1.html", "_blank");
 
     } else {
-        alert("Don't say yes right away, handsome"");
+        alert("Don't say yes right away, handsome");
     }
 }
 
